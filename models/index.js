cf239fbaa91f578f31d24a2d07c3876f49c3583e
christian <christiancarvajal@Christians-MacBook-Air.local>
2022-06-26 18:55:24 -0400

    find all categories route

--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ Product.belongsToMany(Tag,{
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
-TimeRanges.belongsToMany(Product,{
+Tag.belongsToMany(Product,{
   through: ProductTag,
   foreignKey: 'tag_id'
 });
